f1fee8fe2a5661507e94ce965550726d208062f0
thalesfb <15121152+thalesfb@users.noreply.github.com>
2023-04-20 11:46:44 -0300

    feat: get time of worst table

--- src/api_c/testTime_1.c
+++ src/api_c/testTime_1.c
@@ -7,10 +7,41 @@
 
 #define MAX_LEN 256
 
+double get_query_time_unified_table() {
+    MYSQL* CONN = conn();
+    clock_t start = clock();
+    if (mysql_query(CONN, "SELECT * FROM tabela_unificada WHERE status_producao = 'Produzido'") != 0) {
+        fprintf(stderr, "Erro ao executar a consulta: %s\n", mysql_error(CONN));
+        mysql_close(CONN);
+        exit(1);
+    }
+    clock_t end = clock();
+
+    MYSQL_RES* result = mysql_store_result(CONN);
+    if (result == NULL) {
+        fprintf(stderr, "Erro ao armazenar resultado da consulta: %s\n", mysql_error(CONN));
+        mysql_close(CONN);
+        exit(1);
+    }
+
+    MYSQL_ROW row = mysql_fetch_row(result);
+    if (row == NULL) {
+        fprintf(stderr, "Nenhum resultado encontrado.\n");
+        mysql_free_result(result);
+        mysql_close(CONN);
+        exit(1);
+    }
+
+    double timeSpentMS = (double)(end - start) * 1000.0 / CLOCKS_PER_SEC;
+    mysql_close(CONN);
+    mysql_free_result(result);
+    return timeSpentMS;
+}
+
 double get_query_time() {
     MYSQL* CONN = conn();
     clock_t start = clock();
-    if (mysql_query(CONN, "select * from vendas_com_indices where id_venda = 9999") != 0) {
+    if (mysql_query(CONN, "select * from produzidos") != 0) {
         fprintf(stderr, "Erro ao executar a consulta: %s\n", mysql_error(CONN));
         mysql_close(CONN);
         exit(1);
